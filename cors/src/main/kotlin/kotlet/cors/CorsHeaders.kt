811b30c0d7ae6e4dd8889a2b421e3a6dce9e89cf
Aleksandr Turchenko <aleksandr.turchenko@lightspeedhq.com>
2024-07-03 19:02:46 +0400

    Fix cors

--- cors/src/main/kotlin/kotlet/cors/CorsHeaders.kt
+++ cors/src/main/kotlin/kotlet/cors/CorsHeaders.kt
@@ -8,13 +8,15 @@ data class CorsHeaders(
     val allowOrigin: String,
 
     /**
-     * The Access-Control-Allow-Methods response header specifies one or more methods allowed when accessing a resource in response to a preflight request.
+     * The Access-Control-Allow-Methods response header specifies one or more methods allowed when accessing a resource in response to a
+     * preflight request.
      * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Methods
      */
     val allowMethods: String,
 
     /**
-     * The Access-Control-Allow-Headers response header is used in response to a preflight request which includes the Access-Control-Request-Headers to indicate which HTTP headers can be used during the actual request.
+     * The Access-Control-Allow-Headers response header is used in response to a preflight request which includes the
+     * Access-Control-Request-Headers to indicate which HTTP headers can be used during the actual request.
      * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
      */
     val allowHeaders: String
