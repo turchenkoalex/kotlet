ea4059b06402e25d199cf8c8e61d01d6dd56d1e9
Aleksandr Turchenko <aleksandr.turchenko@lightspeedhq.com>
2024-11-28 21:37:49 +0400

    Add Swagger UI lib

---
+++ swagger-ui/src/main/kotlin/swagger/ui/SwaggerUIConfig.kt
@@ -0,0 +1,31 @@
+package swagger.ui
+
+/**
+ * Configuration for the Swagger UI endpoint
+ */
+internal data class SwaggerUIConfig(
+    val path: String,
+    val openAPIPath: String,
+)
+
+/**
+ * Builder for [SwaggerUIConfig].
+ */
+class SwaggerUIConfigBuilder internal constructor() {
+    /**
+     * Path to the Swagger UI endpoint
+     */
+    var path: String = "/swagger/ui"
+
+    /**
+     * Path to the OpenAPI endpoint
+     */
+    var openAPIPath: String = "/openapi.json"
+
+    internal fun build(): SwaggerUIConfig {
+        return SwaggerUIConfig(
+            path = path,
+            openAPIPath = openAPIPath,
+        )
+    }
+}
