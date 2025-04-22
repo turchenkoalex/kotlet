b4e29ee036638cd00fcf8d164b48482ba44f460b
Aleksandr Turchenko <turchenko.alexander@gmail.com>
2025-04-22 08:43:51 +0400

    Add interceptor order parameter to metrics installation and enhance MetricsCollector interface

--- metrics/src/main/kotlin/kotlet/metrics/Metrics.kt
+++ metrics/src/main/kotlin/kotlet/metrics/Metrics.kt
@@ -1,5 +1,6 @@
 package kotlet.metrics
 
+import kotlet.InstallOrder
 import kotlet.Interceptor
 import kotlet.Routing
 
@@ -12,7 +13,17 @@ object Metrics {
 /**
  * Install metrics collection as global routing interceptor
  */
-fun Routing.installMetrics(metricsCollector: MetricsCollector) {
+fun Routing.installMetrics(
+    /**
+     * Implementation of [MetricsCollector] to collect metrics
+     */
+    metricsCollector: MetricsCollector,
+
+    /**
+     * Order of the interceptor in the chain
+     */
+    order: InstallOrder = InstallOrder.LAST,
+) {
     val interceptor = Metrics.interceptor(metricsCollector)
-    install(interceptor)
+    install(interceptor, order = order)
 }
