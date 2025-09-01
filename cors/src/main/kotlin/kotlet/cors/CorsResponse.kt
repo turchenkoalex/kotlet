564614081db80b792bfece331d9ff683ed0ae9fe
Aleksandr Turchenko <turchenko.alexander@gmail.com>
2025-09-01 09:09:49 +0400

    Refactor comments in CorsResponse.kt for improved readability

--- cors/src/main/kotlin/kotlet/cors/CorsResponse.kt
+++ cors/src/main/kotlin/kotlet/cors/CorsResponse.kt
@@ -48,23 +48,23 @@ sealed class CorsResponse {
          */
         fun headers(
             /**
-             * The Access-Control-Allow-Origin response header indicates whether the response can be shared with requesting
-             * code from the given origin.
+             * The Access-Control-Allow-Origin response header indicates whether the response can be shared with
+             * requesting code from the given origin.
              * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
              */
             allowOrigin: String,
 
             /**
-             * The Access-Control-Allow-Methods response header specifies one or more methods allowed when accessing a
-             * resource in response to a preflight request.
+             * The Access-Control-Allow-Methods response header specifies one or more methods allowed when accessing
+             * a resource in response to a preflight request.
              * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Methods
              */
             allowMethods: List<String>,
 
             /**
-             * The Access-Control-Allow-Headers response header is used in response to a preflight request which includes
-             * the Access-Control-Request-Headers to indicate which HTTP headers can be used during the actual request.
-             * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
+             * The Access-Control-Allow-Headers response header is used in response to a preflight request which
+             * includes the Access-Control-Request-Headers to indicate which HTTP headers can be used during the
+             * actual request. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
              */
             allowHeaders: List<String>,
 
