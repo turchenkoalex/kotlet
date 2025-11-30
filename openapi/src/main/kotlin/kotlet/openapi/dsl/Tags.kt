e9658cd123ec2ed90f6dbe51b0448ec7ed99b172
Aleksandr Turchenko <turchenko.alexander@gmail.com>
2025-11-30 21:27:37 +0400

    Remove tags extension

--- openapi/src/main/kotlin/kotlet/openapi/dsl/Tags.kt
+++
@@ -1,9 +0,0 @@
-package kotlet.openapi.dsl
-
-import io.swagger.v3.oas.models.Operation
-
-var Operation.tags: List<String>
-    get() = this.tags
-    set(value) {
-        this.tags = value.toList()
-    }
